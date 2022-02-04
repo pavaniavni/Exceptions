800c772fdac851af1dc15d5f0b54afabc13ae143
pavani <pavaniavni456@gmail.com>
2022-02-04 17:23:51 +0530

    added exceptions

---
+++ Exceptions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleadClass
+{
+
+    class Exception
+    {
+        public static void Main()
+        {
+            try
+            {
+                
+                string val = null;
+                int res = int.Parse(val);
+            }
+            
+            catch (ArgumentNullException ex2)
+            {
+                Console.WriteLine("Argument null exception "+ ex2.Message);
+            }
+
+
+            Console.WriteLine();
+        }
+    }
+}
